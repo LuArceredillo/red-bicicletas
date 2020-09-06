c067f79ec6de462cdf088252c0df8cb51aebc8f2
Lucila Arceredillo <lucila_arceredillo@hotmail.com>
2020-09-06 18:43:56 -0300

    models

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -13,6 +13,27 @@ Bicicleta.allBicis = [];
 Bicicleta.add = function(aBici){
     Bicicleta.allBicis.push(aBici);
 }
+
+Bicicleta.findById = function(aBiciId) {
+    var aBici = Bicicleta.allBicis.find( x => x.id == aBiciId );
+    if(aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicicleta con el ID:  ${aBiciId}`)
+}
+
+Bicicleta.removeById = function(aBiciId) {
+    for(var i=0; i< Bicicleta.allBicis.length ; i++){
+        if(Bicicleta.allBicis[i].id == aBiciId){
+            Bicicleta.allBicis.splice(i,1);
+            break;
+        }
+    }
+   
+}
+
+
+
 var a = new Bicicleta(1, 'rojo', '29', [-34.60,-58.38]);
 var b = new Bicicleta(2, 'azul', '29', [-34.60,-58.38]);
 
